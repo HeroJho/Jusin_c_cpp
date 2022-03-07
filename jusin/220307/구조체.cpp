a28db59101de9450116cf18c0759c34215baaca0
HeroJho <theherojho@naver.com>
2022-03-07 23:30:42 +0900

    구조체

---
+++ jusin/220307/구조체.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+// 키워드-자료형의 이름
+struct tagInfo     // 24바이트
+{
+	bool bChoice;
+	char szName[64];		// 멤버 변수
+	int iA;					// 멤버 변수
+	float fB;				// 멤버 변수
+	double dbTemp;
+};
+
+struct tegTemp
+{
+	char cName;
+	short sBB;
+	int iAA;
+	long long llCC;
+
+	tagInfo tInfo;	// 사용자 정의 자료형은 구조체 메모리 할당 정책을 따르지 않는다.
+};
+
+void main()
+{
+	// 구조체 : 여러 자료형을 한데 묶어서 관리하는 사용자 정의 자료형
+	// 사용자 정의 자료형: 사용자가 직접 자료형을 만들어 사용한다는 것, 기본 자료형을 조합해서
+	//    ㄴ 구조체, 열거체, 공용체
+	//    ㄴ c++은 클래스 추가
+
+	{
+		// 구조체 선언
+		tagInfo tInfo;
+
+		cout << "이름 입력 : ";
+		cin >> tInfo.szName;		// . 멤버접근 연산자
+
+		cout << "int형 변수 입력: ";
+		cin >> tInfo.iA;
+
+		cout << tInfo.szName << endl;
+		cout << tInfo.iA << endl;
+	}
+
+	{
+		// 구조체 초기화
+		// 멤버 변수가 선언된 순서로 초기화
+		tagInfo tInfo = { "홍길동", 100, 6.27 };
+		tagInfo tInfo2 = {  };
+		cout << tInfo.szName << endl;
+	}
+
+	{
+		// 구조체 크기
+		// 메모리할당할 때 멤버 자료형중 가장 큰 자료형을 기준으로 메모리 블럭을 잡는다.
+		// 메모리 블럭에 멤버 공간을 할당하고 부족하면 메모리 블럭만큼 또 할당.
+		// 메모리 블럭은 기본자료형만 해당된다.
+		tagInfo tInfo = {};
+		cout << sizeof(tagInfo);
+	}
+
+	{
+		// 구조체도 포인터 가능! 똑같이 4바이트
+		tagInfo tInfo = {};
+		tagInfo* ptInfo = &tInfo;
+		
+		(*ptInfo).fB;
+		ptInfo->fB;
+	}
+}
+
+// 숙제: 성적 프로그램 구조체 사용해서 만들기
---
+++ jusin/220307Assignments/220307Assignments.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+using namespace std;
+
+const int MAX_COUNT = 5;	// N명의 데이터 저장
+const int MAX_LEN = 256;
+
+struct StudentInfo
+{
+	char cName[MAX_LEN] = {};
+	int iKor;
+	int iEng;
+	int iMath;
+	int iTotal;
+	float fAvg;
+};
+
+void InputInfo(StudentInfo* psuts, int* iCount)
+{
+	if (*iCount > MAX_COUNT - 1)
+	{
+		cout << "데이터 초과" << endl;
+		system("pause");
+		return;
+	}
+
+	cout << "이름: ";
+	cin >> psuts[*iCount].cName;
+	cout << "국어: ";
+	cin >> psuts[*iCount].iKor;
+	cout << "영어: ";
+	cin >> psuts[*iCount].iEng;
+	cout << "수학: ";
+	cin >> psuts->iMath;
+	
+	psuts[*iCount].iTotal = psuts[*iCount].iKor + psuts[*iCount].iEng + psuts[*iCount].iMath;
+	psuts[*iCount].fAvg = (float)psuts[*iCount].iTotal / 3;
+
+	++(*iCount);
+}
+void PrintInfos(StudentInfo* psuts, int* iCount)
+{
+	system("cls");
+
+	for (int i = 0; i < *iCount; i++)
+	{
+		cout << "이름: " << psuts[i].cName << "  " << "국어: " << psuts[i].iKor << "  " << "영어: " << psuts[i].iEng << "  " << "수학: " << psuts[i].iMath << "  " << "총점: " << psuts[i].iTotal << "  " << "평균: " << psuts[i].fAvg << endl;
+	}
+
+	system("pause");
+}
+void FindByNameAndPrintInfo(StudentInfo* psuts, int* iCount)
+{
+	system("cls");
+
+	bool isFind = false;
+	StudentInfo findedInfo = {};
+	char cTemp[MAX_LEN] = {};
+	cout << "찾으실 이름을 입력하세요. >> ";
+	cin >> cTemp;
+	for (int i = 0; i < *iCount; i++)
+	{
+		if (strcmp(psuts[i].cName, cTemp)) // 다르면
+			continue;
+		isFind = true;
+		findedInfo = psuts[i];
+	}
+
+	if (isFind)
+		cout << "이름: " << findedInfo.cName << "  " << "국어: " << findedInfo.iKor << "  " << "영어: " << findedInfo.iEng << "  " << "수학: " << findedInfo.iMath << "  " << "총점: " << findedInfo.iTotal << "  " << "평균: " << findedInfo.fAvg << endl;
+	else
+		cout << "찾으시는 정보는 없습니다..." << endl;
+
+	system("pause");
+}
+
+void main()
+{
+	StudentInfo studentInfos[MAX_LEN] = {};
+	void(*Funcs[3])(StudentInfo*, int*) = { InputInfo, PrintInfos, FindByNameAndPrintInfo };
+	int iCount = 0;
+	int iInput = 0;
+
+	while (true)
+	{
+		system("cls");
+		cout << "1.입력 2.전체 출력 3.검색 출력(이름) 4.종료 >> ";
+		cin >> iInput;
+
+		if (4 == iInput)
+		{
+			cout << "종료합니다." << endl;
+			system("pause");
+			return;
+		}
+		else if (4 < iInput)
+		{
+			cout << "다시 입력해 주세요." << endl;
+			system("pause");
+			continue;
+		}
+
+		Funcs[iInput-1](studentInfos, &iCount);
+	}
+}
