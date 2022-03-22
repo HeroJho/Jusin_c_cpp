65a3cf3060c316ea0c11768b459314551bfd7477
HeroJho <theherojho@naver.com>
2022-03-22 13:30:22 +0900

    생성자와 소멸자

--- jusin2/220321Assignments/StudentManager.h
+++ jusin2/220321Assignments/StudentManager.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "Student.h"
 #include <iostream>
 
--- jusin2/220321Assignments_RPG/main.cpp
+++ jusin2/220321Assignments_RPG/main.cpp
@@ -1,5 +1,4 @@
-﻿#include <iostream>
-#include <string>
+﻿#include <string>
 using namespace std;
 
 #include "CGameManager.h"
