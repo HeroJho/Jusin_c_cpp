724b2fe20417a071cf680f233fda533e55fa4db4
HeroJho <theherojho@naver.com>
2022-05-14 23:24:50 +0900

    Test

--- jusin/Test/Test1.cpp
+++ jusin/Test/Test1.cpp
@@ -5,8 +5,25 @@
 
 using namespace std;
 
+class Player
+{
+public:
+	void Update()
+	{
+		delete this;
+
+		cout << "AAAA" << endl;
+	}
+};
+
 void main()
 {
+	Player* p = new Player;
+
+	while (true)
+	{
+		p->Update();
+	}
 }
 
 
